8a34362cb42746bf9721f74a85f32dbeb4fb342f
nahuerodriguez94 <mna.rodriguez94@gmail.com>
2021-04-04 17:23:13 -0300

    Ejercicio Stock

---
+++ stock.php
@@ -0,0 +1,11 @@
+<?php
+$stock= "800";
+//Si el stock es mayor que 0 imprimir "Hay stock"
+if ($stock > 0) {
+    echo "Hay stock";
+}  //si el stock es 0 imprimir "No hay stock
+else {
+    echo "No hay stock";
+}
+
+?>
